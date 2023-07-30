761840d2418e0cf7216bae03559e581bc67706b0
akshaychandel1 <chandel2102@gmail.com>
2023-07-30 17:57:40 +0530

    Portfolio

---
+++ main.js
@@ -0,0 +1,12 @@
+// var typed = new typed(".typing",{
+// strings: ["Frontend Developer", "UI/UX Designer", "Web Developer"],
+// typespeed: 100,
+// backspeed:100,
+// // backDelay:100,
+// loop:true,
+// })
+
+// var typed = new Typed('#typing', {
+//     strings: ["Frontend Developer", "UI/UX Designer", "Web Developer" ],
+//     typeSpeed: 50,
+//   });
